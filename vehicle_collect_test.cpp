6335e771451c2e5c2ea7242b1c791870d11e9da2
TaraqB <68851196+TaraqB@users.noreply.github.com>
2020-07-31 18:10:05 +0530

    Update vehicle_collect_test.cpp

--- vehicle_collect_test.cpp
+++ vehicle_collect_test.cpp
@@ -1,10 +1,12 @@
 #include <gtest/gtest.h>
 
-TEST(COLLECT,){
-
+TEST(COLLECT,when_vehicle_reports_measurement_fetch_and_store){
+ 
+ ASSERT_EQ(get_vehicle_measurements({vehicleId,measuremntKey,batteryPc}),EN_DATA_AVAILABLE);
 }
 
  
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
+}
