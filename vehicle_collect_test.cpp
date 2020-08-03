adce8467c2d5408079bd3a6c220307ce959237c5
TaraqB <68851196+TaraqB@users.noreply.github.com>
2020-08-03 16:58:19 +0530

    Update vehicle_collect_test.cpp

--- vehicle_collect_test.cpp
+++ vehicle_collect_test.cpp
@@ -1,8 +1,15 @@
 #include <gtest/gtest.h>
+#include "vehicle_collect.h"
 
 TEST(COLLECT,when_vehicle_reports_measurement_fetch_and_store){
  
- ASSERT_EQ(get_vehicle_measurements({vehicleId,measuremntKey,batteryPc}),EN_DATA_AVAILABLE);
+ st_telematics obj_telematics = {231,motor_temp,30};
+ st_inventory obj_inventory   = telematics_to_inventory(&obj_telematics);
+ 
+ ASSER_EQ(obj_inventory.motor_temp,30);
+ 
+  
+ //ASSERT_EQ(get_vehicle_measurements({vehicleId,measuremntKey,batteryPc}),EN_DATA_AVAILABLE);
 }
 
  
