96d73138956344abb6d671abf5004e5d1414ed4e
TaraqB <68851196+TaraqB@users.noreply.github.com>
2020-08-03 17:45:01 +0530

    Update vehicle_collect.cpp

--- vehicle_collect.cpp
+++ vehicle_collect.cpp
@@ -12,10 +12,10 @@ st_inventory telematics_to_inventory(st_telematics* obj_telematics)
       st_inventory.motor_temp = obj_telematics->measurement;
       break;
     case battery_pc:
-      st_inventory.battery_temp = obj_telematics->measurement;
+      st_inventory.battery_pc = obj_telematics->measurement;
       break;
     case battery_temp:
-      st_inventory.battery_pc = obj_telematics->measurement;
+      st_inventory.battery_temp = obj_telematics->measurement;
       break;
     default:break;
   }
